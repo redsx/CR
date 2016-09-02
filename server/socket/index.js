e016fdaf8b4e95e0079f08c89631f0fdcc04b6de
condy <15979063786@163.com>
2016-09-02 14:00:21 +0800

    socket

--- server/socket/index.js
+++ server/socket/index.js
@@ -5,57 +5,113 @@ const co = require('co')
     , connect = require('../controllers/connect');
 module.exports = function (io) {
     io.on('connect',function (socket) {
+        //设置TCP连接超时时间
+        socket.on('login',(info,cb) => {
+            console.log(info.nickname + 'login');
+            co(user.verifyUser(info.nickname,info.password,cb)).catch((err) =>{
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
+                console.log(err);
+            });
+        })
+        socket.on('signUp',(info,cb) => {
+            co(user.createUser(info.nickname,info.password,cb)).catch((err) =>{
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
+                console.log(err);
+            });
+        })
         socket.on('getInfo',(token,cb) => {
             co(connect.init(token,socket,io,cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('getOnlineUsers',(cb) => {
             co(online.onlineUsers(cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('message',(msg,cb) => {
             co(message.getMessage(msg,socket,cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('privateMessage',(msg,cb)=>{
             co(message.getPrivateMessage(msg,socket,cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('history',(room,cb) => {
             co(message.getHistoryMessage(room,cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('getUserInfo',(nickname,cb) => {
             co(user.getUserInfo(nickname,cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
+                console.log(err);
+            });
+        });
+        socket.on('updateUserInfo',(userInfo,cb) => {
+            co(user.updateUserInfo(userInfo,cb)).catch((err) =>{
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('changeAvatar',(info,cb) => {
-            co(user.changeUserInfo(info,io,cb)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+            co(user.updateUserAvatar(info,io,cb)).catch((err) =>{
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         });
         socket.on('disconnect',() => {
             co(connect.leave(socket,io)).catch((err) =>{
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         })
-        socket.on('reconnect_success',(token)=>{
-            console.log('断线重连');
-            co(connect.reconnect(token)).catch((err) =>{
-                cb({isError:'服务器开了个小差'});
+        socket.on('reconnect success',(token)=>{
+            co(connect.reconnect(token,io)).catch((err) =>{
+                cb({
+                    isError:true,
+                    errMsg: '服务器开了个小差'
+                });
                 console.log(err);
             });
         })
