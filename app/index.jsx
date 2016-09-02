b273c5273682e77fec4dd1a70dfb4579c67c8062
condy <15979063786@163.com>
2016-09-02 13:50:17 +0800

    index

--- app/index.jsx
+++ app/index.jsx
@@ -1,11 +1,15 @@
 import 'babel-polyfill'
 
 import React from 'react'
+import { browserHistory, hashHistory, Router, Route, IndexRoute } from 'react-router'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
-import App from './containers/App.js'
+import App from './components/App.jsx'
+import Login from './pages/login/Login.jsx'
+import SignUp from './pages/signup/SignUp.jsx'
+import Index from './pages/index/'
 
 import store from './store'
 
@@ -31,85 +35,13 @@ import {
 import notification from './actions/notification.js'
 import favico from './actions/favicoNotification.js'
 
+//test Immutable
+import Immutable from 'immutable'
+
 favico.resetWhenDocVisibility();
 notification.requestPermission();
 
-socket.on('privateMessage', (message) => {
-    console.log('get private message:',message);
-    const state = store.getState();
-    if(state.setting.shield.indexOf(message.nickname) === -1){
-        const audio = document.getElementById('audio1'),
-              audioSpecial = document.getElementById('audio3'),
-              avatar = state.onlineUsers[message.nickname].avatar;
-        message.room = message.nickname;
-        store.dispatch(addPrivateMessage(message));
-        state.userState.curRoom === message.room ? null : store.dispatch(addCount(message.room));
-        if(document.hidden){
-            favico.addBage();            
-            state.setting.h5Notification ? notification.showNotification(message.nickname,{
-                body: message.content,
-                icon: (store.getState()).onlineUsers[message.nickname].avatar,
-            }) : null;
-            if(!state.setting.audio.isClose){
-                state.setting.special.indexOf(message.nickname) === -1 ? audio.play() : audioSpecial.play();
-            }
-        } else if(!state.setting.audio.isClose && state.userState.curRoom !== message.room){
-            state.setting.special.indexOf(message.nickname) === -1 ? audio.play() : audioSpecial.play();
-        }
-    }
-});
-socket.on('newMessage', (message) => {
-    console.log('get new message',message);
-    const state = store.getState();
-    if(state.setting.shield.indexOf(message.nickname) === -1){
-        const audio = document.getElementById('audio1'),
-              audioSpecial = document.getElementById('audio3'),
-              avatar = state.onlineUsers[message.nickname].avatar,
-              reg = eval('/'+'@'+state.userState.nickname+'/g');
-        store.dispatch(addMessage(message));
-        state.userState.curRoom === message.room ? null : store.dispatch(addCount(message.room));
-        if(document.hidden){
-            favico.addBage();
-            state.setting.h5Notification && !(message.nickname === state.userState.nickname) ? notification.showNotification(message.nickname,{
-                body: message.content,
-                icon: (store.getState()).onlineUsers[message.nickname].avatar,
-            }) : null;
-            if(!state.setting.audio.isClose){
-                state.setting.special.indexOf(message.nickname) !== -1 || reg.test(message.content) ? audioSpecial.play() : audio.play() ;
-            }
-        } else if(!(message.nickname === state.userState.nickname) && !state.setting.audio.isClose && state.userState.curRoom !== message.room){
-            state.setting.special.indexOf(message.nickname) !== -1 || reg.test(message.content) ? audioSpecial.play() : audio.play();
-        }
-    }
-});
-
-socket.on('changeInfo', (info)=>{
-    console.log('change info:',info);
-    store.dispatch(changeUserInfo(info));
-})
-socket.on('user joined', (user) => {
-    console.log('user joined:',user.nickname);
-    // store.dispatch(addMessage({
-    //     content: user.nickname+'加入了',
-    //     room: 'MDZZ',
-    //     type: 'systemMessage'
-    // }));
-    store.dispatch(addOnlineUser(user));
-})
-socket.on('user leaved', (user) => {
-    console.log('user leaved:',user.nickname);
-    // store.dispatch(addMessage({
-    //     content: user.nickname+'离开了',
-    //     room: 'MDZZ',
-    //     type: 'systemMessage'
-    // }));
-    store.dispatch(deleteLogoutUser(user.nickname));
-})
-socket.on('connect', () => {
-    const token = localStorage.getItem('token');
-    if(!token){
-        window.location = '/login';
-    }
+const handleInit = (token) => {
     getHistoryMessage('MDZZ')(store.dispatch).then((resault)=>{
         return getInitOnlineUser()(store.dispatch)
     }).then((resault)=>{
@@ -120,10 +52,7 @@ socket.on('connect', () => {
         let setting = storage.setting;
         if(setting){
             store.dispatch(setNotificationState(setting.h5Notification));
-            if(setting.audio){
-                store.dispatch(setAudioSrc(setting.audio.src));
-                store.dispatch(setAudioState(!!(setting.audio.isClose)));
-            }
+            store.dispatch(setAudioState(!!(setting.audioNotification)));
             store.dispatch(setShieldUser({
                 user: setting.shield,
                 isAdd: true
@@ -137,10 +66,90 @@ socket.on('connect', () => {
             store.dispatch(initStorageExpression(storage.expressions));
         }
     })
+}
+const handleEnter = (nextState,replace) => {
+    const token = localStorage.getItem('token');
+    if(token){
+        return handleInit(token);
+    } else{
+        replace({pathname: '/login'});
+    }
+}
+const handleLeave = () => {
+    const token = localStorage.getItem('token');
+    if(token){
+        socket.emit('reconnect success',token);
+    }
+}
+socket.on('privateMessage', (message) => {
+    const state = store.getState().toJS();
+    if(state.setting.shield.indexOf(message.nickname) === -1){
+        const audio = document.getElementById('audio1'),
+              audioSpecial = document.getElementById('audio3'),
+              avatar = state.onlineUsers[message.nickname].avatar;
+        message.room = message.nickname;
+        store.dispatch(addPrivateMessage(message));
+        state.userState.curRoom === message.room ? null : store.dispatch(addCount(message.room));
+        if(document.hidden){
+            favico.addBage();        
+            if(state.setting.audioNotification){
+                state.setting.special.indexOf(message.nickname) === -1 ? audio.play() : audioSpecial.play();
+            }    
+            state.setting.h5Notification ? notification.showNotification(message.nickname,{
+                body: message.content,
+                icon: state.onlineUsers[message.nickname].avatar,
+            }) : null;
+        } else if(state.setting.audioNotification && state.userState.curRoom !== message.room){
+            state.setting.special.indexOf(message.nickname) === -1 ? audio.play() : audioSpecial.play();
+        }
+    }
+});
+socket.on('newMessage', (message) => {
+    const state = store.getState().toJS();
+    if(state.setting.shield.indexOf(message.nickname) === -1){
+        const audio = document.getElementById('audio1'),
+              audioSpecial = document.getElementById('audio3'),
+              avatar = state.onlineUsers[message.nickname].avatar,
+              reg = eval('/'+'@'+state.userState.nickname+'/g');
+        store.dispatch(addMessage(message));
+        state.userState.curRoom === message.room ? null : store.dispatch(addCount(message.room));
+        if(document.hidden){
+            favico.addBage();
+            if(state.setting.audioNotification){
+                state.setting.special.indexOf(message.nickname) !== -1 || reg.test(message.content) ? audioSpecial.play() : audio.play() ;
+            }
+            state.setting.h5Notification && !(message.nickname === state.userState.nickname) ? notification.showNotification(message.nickname,{
+                body: message.content,
+                icon: state.onlineUsers[message.nickname].avatar,
+            }) : null;
+        } else if(!(message.nickname === state.userState.nickname) && state.setting.audioNotification && state.userState.curRoom !== message.room){
+            state.setting.special.indexOf(message.nickname) !== -1 || reg.test(message.content) ? audioSpecial.play() : audio.play();
+        }
+    }
+});
+
+socket.on('changeInfo', (info)=>{
+    store.dispatch(changeUserInfo(info));
+})
+socket.on('user joined', (user) => {
+    const state = store.getState().toJS();
+    const audio = document.getElementById('audio2');
+    store.dispatch(addOnlineUser(user));
+    if(document.hidden){
+        if(state.setting.audioNotification){
+            audio.play() ;
+        }
+    } 
+})
+socket.on('user leaved', (user) => {
+    store.dispatch(deleteLogoutUser(user.nickname));
+})
+socket.on('connect', () => {
+    
 })
 socket.on('disconnect',()=>{
     console.log('disconnect');
-    const state = store.getState();
+    const state = store.getState().toJS();
     store.dispatch(addMessage({
         content: '掉线重连中...',
         room: state.userState.curRoom,
@@ -156,9 +165,10 @@ socket.on('reconnect',()=>{
     console.log('断线重连成功');
     const token = localStorage.getItem('token');
     if(!token){
-        window.location = '/login';
+        return browserHistory.push('/login')
     }
-    socket.emit('reconnect_success',token);
+    socket.emit('reconnect success',token);
+    handleInit(token);
 })
 socket.on('reconnecting',()=>{
     reconnect++;
@@ -168,7 +178,15 @@ socket.on('reconnecting',()=>{
 injectTapEventPlugin();
 render(
     <Provider store={store}>
-        <App />
+        <div>
+            <Router history = {browserHistory}>
+                <Route path = '/' component = {App} >
+                    <IndexRoute component= {Index} onEnter = {(nextState,replace)=>handleEnter(nextState,replace)} onLeave = {()=>handleLeave()}/>
+                    <Route path= 'login' component= {Login} />
+                    <Route path= 'signUp' component= {SignUp} />
+                </Route>
+            </Router>
+        </div>
     </Provider>
     ,
     document.getElementById('App')
