8606cd67c14c7fb2d2c0524afc8a19f8db10a3ca
condy <15979063786@163.com>
2016-10-11 19:57:10 +0800

    config

--- server/config/cr-config.js
+++ server/config/cr-config.js
@@ -1,6 +1,8 @@
 module.exports = {
-    ROBOT_NAME : '机器人小z',
+    ROBOT_NAME: '机器人小z',
     INIT_ROOM: 'MDZZ',
+    INIT_ROOM_INFO: '有问题欢迎提issue',
+    JWT_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoibWQiLCJleHAiOjE0Nzc2NTA1MTksImlhdCI6MTQ3NTA1ODUxOX0.p2pN8HVHj73-Ytvlen_k3LXkkAkh73MU7kEqZ73V',
     ROBOT_URL: 'http://www.tuling123.com/openapi/api',
     ROBOT_KEY: '273e3ca4189a3fc03b4a94db8d87796b',
     ROBOT_AVATAR:'http://oajmk96un.bkt.clouddn.com/robot.jpg',
