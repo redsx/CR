7918aedbb15d859f0b0439ed08fe1741a315607f
condy <15979063786@163.com>
2016-10-11 19:59:02 +0800

    app

--- server/app.js
+++ server/app.js
@@ -3,12 +3,16 @@ const app = require('koa')()
   , co = require('co')
   , logger = require('koa-logger')
   , json = require('koa-json')
-  , views = require('koa-views')
+  , views = require('koa-views')    
+  , bluebird = require('bluebird')
   , onerror = require('koa-onerror');
 
 const index = require('./routes/index')
    , db = require('./models/db-mongo')
-   , Online = require('./models/online-mongo')    
+   , Online = require('./models/online-mongo')
+   , Room = require('./models/room-mongo')    
+   , User = require('./models/user-mongo')
+   , History = require('./models/history-mongo')
    , message = require('./controllers/message')
    , users = require('./routes/users')
    , login = require('./routes/login')
@@ -16,13 +20,21 @@ const index = require('./routes/index')
    , config = require('./config/config');
 
 
+
 co(function *() {
-  yield Online.removeAll();
-  yield Online.createOnline({
-    nickname:crConfig.ROBOT_NAME,
-    socket:'robot',
-    avatar:crConfig.ROBOT_AVATAR
-  });
+    let initRoom = yield Room.findOne({name: crConfig.INIT_ROOM});
+    if(!initRoom){
+        let room = new Room({
+            info: crConfig.INIT_ROOM_INFO,
+            name: crConfig.INIT_ROOM
+        })
+        room.save();
+    } else{
+      console.log('初始房间已存在');
+    }
+    yield Online.removeAll();
+}).catch((err)=>{
+    console.log(err);
 });
 
 
