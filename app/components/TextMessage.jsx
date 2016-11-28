2fe816fe3a4756fb464157043b24284b443fa0df
condy <15979063786@163.com>
2016-11-28 08:40:50 +0800

    feat: 加入alt属性，复制聊天内容时可以连表情一同复制

--- app/components/TextMessage.jsx
+++ app/components/TextMessage.jsx
@@ -18,7 +18,7 @@ class TextMessage extends React.Component{
         text = text.replace(/&/g,'&amp').replace(/\"/g,'&quot').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/\'/g,'&apos;');
         let regLink = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
         let regExp = /#\([\u4e00-\u9fa5a-z]+\)/g;
-        text = text.replace(regExp, r => `<img src="images/expressions/${r.match(/[^#()]+/)[0]}.png" onerror="this.style.display=\'none\'"/>` );
+        text = text.replace(regExp, r => `<img src="images/expressions/${r.match(/[^#()]+/)[0]}.png" alt="${r}" onerror="this.style.display=\'none\'"/>` );
         text = text.replace(regLink, r => `<a href="${r}" target="_blank">${r}</a>`);
         return text
     }
